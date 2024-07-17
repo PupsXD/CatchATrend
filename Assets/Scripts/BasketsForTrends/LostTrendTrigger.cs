1ba003efd7236dfca7e0e95dd94aaa392c943e98
Petr Sokolov <74403997+PupsXD@users.noreply.github.com>
2024-07-17 20:29:08 +0300

    Fixed losing a trend in void

---
+++ Assets/Scripts/BasketsForTrends/LostTrendTrigger.cs
@@ -0,0 +1,15 @@
+﻿using DefaultNamespace;
+using Trends;
+
+namespace BasketsForTrends
+{
+    public class LostTrendTrigger: Basket
+    {
+        public override void AcceptTrends(Trend trend)
+        {
+            accseptedTrend.Add(trend);
+            
+            
+        }
+    }
+}
